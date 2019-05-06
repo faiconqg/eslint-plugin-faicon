351a77a7add2b2f37321c26f676e7dd3093ec6c1
thiagolins <thiagolins@faicon.com>
2019-05-06 20:01:32 -0300

    Agora funcionando

--- tests/lib/rules/no-unused-styles.js
+++ tests/lib/rules/no-unused-styles.js
@@ -30,265 +30,166 @@ ruleTester.run("no-unused-styles", rule, {
   valid: [
     {
       code: `
-        const styles = theme => ({
-          name: {}
-        });
-        const Hello = React.createClass({
-          render: function() {
-            return <Text textStyle={styles.name}>Hello {this.props.name}</Text>;
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const Hello = React.createClass({
-          render: function() {
-            return <Text textStyle={styles.name}>Hello {this.props.name}</Text>;
-          }
-        });
-        const styles = theme => ({
-          name: {}
-        });
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-          name: {}
-        });
-        const Hello = React.createClass({
-          render: function() {
-            return <Text style={styles.name}>Hello {this.props.name}</Text>;
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-          name: {},
-          welcome: {}
-        });
-        const Hello = React.createClass({
-          render: function() {
-            return <Text style={styles.name}>Hello {this.props.name}</Text>;
-          }
-        });
-        const Welcome = React.createClass({
-          render: function() {
-            return <Text style={styles.welcome}>Welcome</Text>;
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-          text: {}
-        })
-        const Hello = React.createClass({
-          propTypes: {
-            textStyle: Text.propTypes.style,
-          },
-          render: function() {
-            return <Text style={[styles.text, textStyle]}>Hello {this.props.name}</Text>;
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-          text: {}
-        })
-        const styles2 = StyleSheet.create({
-          text: {}
-        })
-        const Hello = React.createClass({
-          propTypes: {
-            textStyle: Text.propTypes.style,
-          },
-          render: function() {
-            return (
-              <Text style={[styles.text, styles2.text, textStyle]}>
-               Hello {this.props.name}
-              </Text>
-             );
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-          text: {}
-        });
-        const Hello = React.createClass({
-          getInitialState: function() {
-            return { condition: true, condition2: true }; 
-          },
-          render: function() {
-            return (
-              <Text
-                style={[
-                  this.state.condition &&
-                  this.state.condition2 &&
-                  styles.text]}>
-                Hello {this.props.name}
-              </Text>
-            );
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-          text: {},
-          text2: {},
-        });
-        const Hello = React.createClass({
-          getInitialState: function() {
-            return { condition: true }; 
-          },
-          render: function() {
-            return (
-              <Text style={[this.state.condition ? styles.text : styles.text2]}>
-                Hello {this.props.name}
-              </Text>
-            );
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-            style1: {
-                color: 'red',
-            },
-            style2: {
-                color: 'blue',
-            }
-        });
-        export default class MyComponent extends Component {
-            static propTypes = {
-                isDanger: PropTypes.bool
-            };
-            render() {
-                return <View style={this.props.isDanger ? styles.style1 : styles.style2} />;
-            }
+      const styles = theme => ({
+        progress: {},
+        logoDiv: {},
+        logoDivImg: {},
+        logoImg: {},
+        scrollbar: {},
+        appName: {},
+        drawer: {},
+        version: {},
+        divFora: {}
+      })
+      
+      export default
+      @withStyles(styles)
+      @withWidth()
+      @inject('router')
+      @observer
+      class Menu extends Page {
+        render() {
+          const { classes } = this.props
+          const { sidebar } = RealmStore.logos || {}
+          const width = this.resolveWidth()
+          return (
+            <SwipeableDrawer
+              open={AppStore.smallToggled}
+              anchor="left"
+              classes={{ paper: classes.drawer }}
+              variant={this.isSmall() ? 'temporary' : 'permanent'}
+              onClose={() => (AppStore.smallToggled = false)}
+              onOpen={() => (AppStore.smallToggled = true)}
+            >
+              <div style={{ width: width }} className={classes.divFora} onMouseEnter={() => this.toggleMenu(true)} onMouseLeave={() => this.toggleMenu(false)}>
+                {AccessRoutesStore.busy()}
+                <div className={classes.logoDiv} id="logo">
+                  <div className={classes.logoDivImg} onClick={this.handleLogoClick}>
+                    {/*<img
+                      className={classes.logoImg}
+                      style={{
+                        width: width > 69 ? 'auto' : 0
+                      }}
+                      src={logoWhite}
+                      alt="Logo"
+                    />*/}
+                    {sidebar ? (
+                      <img
+                        className={classes.logoImg}
+                        style={{
+                          width: width > 0 ? 'auto' : 0
+                        }}
+                        src={sidebar}
+                        alt="Incentive.me"
+                      />
+                    ) : (
+                      <span className={classes.appName}>
+                        {RealmStore.appName ? (width > 69 ? RealmStore.appName : RealmStore.appName.slice(0, 2)) : width > 69 ? 'Nome do Sistema' : 'NS'}
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <ScrollBar className={classes.scrollbar}>
+                  {AccessRoutesStore.busy() ? (
+                    <LinearLayout center padding={30}>
+                      <CircularProgress className={classes.progress} />
+                    </LinearLayout>
+                  ) : (
+                    <MenuList items={AccessRoutesStore.models} level={0} root onItemClick={this.handleItemClick} path={this.path} />
+                  )}
+                  <span className={classes.version}>{process.env.REACT_APP_VERSION}</span>
+                </ScrollBar>
+              </div>
+            </SwipeableDrawer>
+          )
         }
-      `
-    },
-    {
-      code: `
-        const styles = theme => ({
-          text: {}
-        })
-      `
-    },
-    {
-      code: `
-        const Hello = React.createClass({
-          getInitialState: function() {
-            return { condition: true }; 
-          },
-          render: function() {
-            const myStyle = this.state.condition ? styles.text : styles.text2;
-            return (
-                <Text style={myStyle}>
-                    Hello {this.props.name}
-                </Text>
-            );
-          }
-        });
-        const styles = theme => ({
-          text: {},
-          text2: {},
-        });
-      `
-    },
-    {
-      code: `
-        const additionalStyles = {};
-        const styles = theme => ({
-          name: {},
-          ...additionalStyles
-        });
-        const Hello = React.createClass({
-          render: function() {
-            return <Text textStyle={styles.name}>Hello {this.props.name}</Text>;
-          }
-        });
-      `
-    },
-    {
-      code: `
-        const styles = OtherStyleSheet.create({
-          name: {},
-        });
-        const Hello = React.createClass({
-          render: function() {
-            return <Text textStyle={styles.name}>Hello {this.props.name}</Text>;
-          }
-        });
+      }   
       `
     }
   ],
-
   invalid: [
     {
       code: `
-        const styles = theme => ({
-          text: {}
-        })
-        const Hello = React.createClass({
-          render: function() {
-            return <Text style={styles.b}>Hello {this.props.name}</Text>;
-          }
-        });
+      const styles = theme => ({
+        progress: {},
+        logoDiv: {},
+        logoDiv2: {},
+        logoDivImg: {},
+        logoImg: {},
+        scrollbar: {},
+        appName: {},
+        drawer: {},
+        version: {},
+        divFora: {}
+      })
+      
+      export default
+      @withStyles(styles)
+      @withWidth()
+      @inject('router')
+      @observer
+      class Menu extends Page {
+      
+        render() {
+          const { classes } = this.props
+          const { sidebar } = RealmStore.logos || {}
+          const width = this.resolveWidth()
+          return (
+            <SwipeableDrawer
+              open={AppStore.smallToggled}
+              anchor="left"
+              classes={{ paper: classes.drawer }}
+              variant={this.isSmall() ? 'temporary' : 'permanent'}
+              onClose={() => (AppStore.smallToggled = false)}
+              onOpen={() => (AppStore.smallToggled = true)}
+            >
+              <div style={{ width: width }} className={classes.divFora} onMouseEnter={() => this.toggleMenu(true)} onMouseLeave={() => this.toggleMenu(false)}>
+                {AccessRoutesStore.busy()}
+                <div className={classes.logoDiv} id="logo">
+                  <div className={classes.logoDivImg} onClick={this.handleLogoClick}>
+                    {/*<img
+                      className={classes.logoImg}
+                      style={{
+                        width: width > 69 ? 'auto' : 0
+                      }}
+                      src={logoWhite}
+                      alt="Logo"
+                    />*/}
+                    {sidebar ? (
+                      <img
+                        className={classes.logoImg}
+                        style={{
+                          width: width > 0 ? 'auto' : 0
+                        }}
+                        src={sidebar}
+                        alt="Incentive.me"
+                      />
+                    ) : (
+                      <span className={classes.appName}>
+                        {RealmStore.appName ? (width > 69 ? RealmStore.appName : RealmStore.appName.slice(0, 2)) : width > 69 ? 'Nome do Sistema' : 'NS'}
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <ScrollBar className={classes.scrollbar}>
+                  {AccessRoutesStore.busy() ? (
+                    <LinearLayout center padding={30}>
+                      <CircularProgress className={classes.progress} />
+                    </LinearLayout>
+                  ) : (
+                    <MenuList items={AccessRoutesStore.models} level={0} root onItemClick={this.handleItemClick} path={this.path} />
+                  )}
+                  <span className={classes.version}>{process.env.REACT_APP_VERSION}</span>
+                </ScrollBar>
+              </div>
+            </SwipeableDrawer>
+          )
+        }
+      }   
       `,
       errors: [
         {
-          message: "Unused style detected: styles.text"
-        }
-      ]
-    },
-    {
-      code: `
-        const styles = theme => ({
-          foo: {},
-          bar: {},
-        })
-        class Foo extends React.Component {
-          render() {
-            return <View style={styles.foo}/>;
-          }
-        }
-      `,
-      errors: [
-        {
-          message: "Unused style detected: styles.bar"
-        }
-      ]
-    },
-    {
-      code: `
-        const styles = theme => ({
-          foo: {},
-          bar: {},
-        })
-        class Foo extends React.PureComponent {
-          render() {
-            return <View style={styles.foo}/>;
-          }
-        }
-      `,
-      errors: [
-        {
-          message: "Unused style detected: styles.bar"
+          message: "Unused style detected: styles.logoDiv2"
         }
       ]
     }
